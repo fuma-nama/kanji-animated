23feaef575e3bea38e8f9d15ba762198d1a64796
SonMooSans <god63820869@gmail.com>
2023-10-03 20:58:49 +0800

    Improve lyrics

--- app/lyrics.tsx
+++ app/lyrics.tsx
@@ -5,9 +5,10 @@ import { Test } from "./test";
 export function Lyrics() {
   return (
     <>
-      <Test delay={0} text="漢字アニメーション" />
-      <Test delay={7 * 300} text="死にたいなんて言うなよ" />
-      <Test delay={14 * 300} text="諦めないで生きろよ" />
+      <Test delay={0} text="「死にたいなんて言うなよ。」" />
+      <Test delay={7 * 300} text="「諦めないで生きろよ。」" />
+      <Test delay={14 * 300} text="そんな歌が正しいなんて" />
+      <Test delay={20 * 300} text="馬鹿げてるよな。" />
     </>
   );
 }
