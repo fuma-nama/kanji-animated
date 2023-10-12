175deed89701bb657c26bc45730ae68ad883afb4
SonMooSans <god63820869@gmail.com>
2023-10-12 21:00:46 +0800

    Add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,9 @@ import { Shippori_Mincho } from "next/font/google";
 import clsx from "clsx";
 
 export const metadata: Metadata = {
-  title: "Fuma Nama の",
-  description: "Something cool",
+  title: "Fuma の Music Player",
+  description:
+    "Animation inspired by 命に嫌われている MV (by Iori Kanzaki), based on Canvas API.",
 };
 
 const font = Shippori_Mincho({
